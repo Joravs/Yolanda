bd5264fa8b5dac7d44e09407d777dc7ee5f1f5be
Jordy <jordyl.reyesl@gmail.com>
2024-10-03 13:25:35 +0200

    cambios

--- Trimestre1/Uni2_Pra2/7.php
+++ Trimestre1/Uni2_Pra2/7.php
@@ -1,18 +1,24 @@
 <?php
-    $arraymax=array();
     for($i=0;$i<3;$i++){
         $num=0;
+        $acumulador=0;
         for($j=0;$j<4;$j++){
            echo $matriz[$i][]=rand(1,100) . " ";
            if ($matriz[$i][$j]>$num){
                 $num=$matriz[$i][$j];
             }
+            $acumulador+=$matriz[$i][$j];
         }
-        $arraymax[$i]=$num;
+        $arrayprom[]=$acumulador;
+        $arraymax[]=$num;
         echo "<br>";
     }
     echo "<br>";
     foreach($arraymax as $elemmax){
         echo $elemmax . " "; 
+    }
+    echo "<br>";
+    foreach($arrayprom as $elemprom){
+        echo $elemprom . " "; 
     }
 ?>
