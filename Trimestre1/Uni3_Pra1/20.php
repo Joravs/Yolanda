badcfa61bde47f06885c03a9fece55611e0345fa
Jordy <jordyl.reyesl@gmail.com>
2024-10-07 11:22:04 +0000

    7 oct 2024, 13:22

--- Trimestre1/Uni3_Pra1/20.php
+++ Trimestre1/Uni3_Pra1/20.php
@@ -1,3 +1,8 @@
 <?php
-    
+    $estadios_futbol=array(
+        'Barcelona'=>"Camp Nou",
+        'Real Madrid'=>"Santiago Bernabeu",
+        'Valencia'=>"Mestalla",
+        'Real Sociedad'=>"Anoeta"
+    );
 ?>
