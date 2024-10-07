df010ad2c2327216c6f4969653ab98b4d6c90b2c
Jordy <jordyl.reyesl@gmail.com>
2024-10-07 11:40:52 +0000

    7 oct 2024, 13:40

--- Trimestre1/Uni3_Pra1/21.php
+++ Trimestre1/Uni3_Pra1/21.php
@@ -5,8 +5,8 @@
     'Tienes'=>123,
     'El'=>5,
     'Ejercicio'=>1);
-
-    foreach(asort($numeros) as $numero){
+    asort($numeros);
+    foreach($numeros as $numero){
         echo "$numero ";
     }
 ?>
