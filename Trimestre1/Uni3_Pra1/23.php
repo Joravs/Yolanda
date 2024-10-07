9877cee49945b0b412fb2c39f4c9d3e85f48e2c0
Jordy <jordyl.reyesl@gmail.com>
2024-10-07 11:58:10 +0000

    7 oct 2024, 13:58

--- Trimestre1/Uni3_Pra1/23.php
+++ Trimestre1/Uni3_Pra1/23.php
@@ -1,6 +1,10 @@
 <?php
     $arrayMul=array(
-        'Los Simpson'=>array(),
+        'Los Simpson'=>array(
+            'padre'=>"Homer",
+            'madre'=>"Marge",
+            'hijos'=>array("Bart","Lisa","Maggie")
+        ),
         'Los Griffin'=>array()
     );
 ?>
