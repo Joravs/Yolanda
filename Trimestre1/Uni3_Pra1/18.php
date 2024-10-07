be316c8cbfcde1ed4e3453e362fe62e07f43efd8
Jordy <jordyl.reyesl@gmail.com>
2024-10-07 10:46:46 +0000

    7 oct 2024, 12:46

--- Trimestre1/Uni3_Pra1/18.php
+++ Trimestre1/Uni3_Pra1/18.php
@@ -1,7 +1,11 @@
 <?php
-    require_once "17.php";
+    $array1=array("Lagartija","Araña","Pedro","Gato","Raton");
+    $array2=array("12","34","45","52","12");
+    $array3=array("Sauce","Pino","Naranjo","Chopo","Perro","34");
 
-    $fusiondearrays=array_push($array1,$array2,$array3);
+    $fusiondearrays=array_push($array1);
+    $fusiondearrays=array_push($array2);
+    $fusiondearrays=array_push($array3);
 
     foreach($fusiondearrays as $elem){
         echo "$elem, ";
