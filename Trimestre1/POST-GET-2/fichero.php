bc2ef0a1ccf6708925fdf803408a4ac12bc81fe8
Jordy <jordyl.reyesl@gmail.com>
2024-10-17 12:11:27 +0000

    17 oct 2024, 14:11

--- Trimestre1/POST-GET-2/fichero.php
+++ Trimestre1/POST-GET-2/fichero.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST['nombre'])){
         echo "<script> alert('".$_POST['nombre'] .", ". $_POST['apellidos']."')</script>";
-        unset($_POST);
+        unset($_POST['nombre']);
     }else{
 ?>
 <!DOCTYPE html>
