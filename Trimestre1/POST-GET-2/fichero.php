5a125eaee7724cb345911f1399016a3c240e1cf4
Jordy <jordyl.reyesl@gmail.com>
2024-10-17 11:49:24 +0000

    17 oct 2024, 13:49

---
+++ Trimestre1/POST-GET-2/fichero.php
@@ -0,0 +1,25 @@
+<?php
+    if(isset($_POST['nombre'])){
+        
+    }else{
+        echo '<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <!--Formulario con nombre y apellido-->
+                    <form action="#" method="post">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" id="nombre" name="nombre" required><br><br>
+                        <label for="apellido">Apellidos:</label>
+                        <input type="text" id="apellidos" name="apellidos" required><br><br>
+                        <input type="submit" value="Enviar">
+                    </form>
+                </body>
+                </html>
+                ';
+    }
+?>
