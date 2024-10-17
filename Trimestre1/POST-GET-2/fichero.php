5f5878180b3e8930a4e8a3c49411fa788e485f35
Jordy <jordyl.reyesl@gmail.com>
2024-10-17 11:52:30 +0000

    17 oct 2024, 13:52

--- Trimestre1/POST-GET-2/fichero.php
+++ Trimestre1/POST-GET-2/fichero.php
@@ -2,6 +2,24 @@
     if(isset($_POST['nombre'])){
         include("fichero2.php");
     }else{
-        include("fichero1.html");
+        echo '<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <!--Formulario con nombre y apellido-->
+                    <form action="#" method="post">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" id="nombre" name="nombre" required><br><br>
+                        <label for="apellido">Apellidos:</label>
+                        <input type="text" id="apellidos" name="apellidos" required><br><br>
+                        <input type="submit" value="Enviar">
+                    </form>
+                </body>
+                </html>
+                ';
     }
 ?>
