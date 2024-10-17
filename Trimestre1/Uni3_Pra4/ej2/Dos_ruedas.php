e3c099a085c81b3e17da138e1b81427d94644533
Jordy <jordyl.reyesl@gmail.com>
2024-10-17 10:12:23 +0000

    17 oct 2024, 12:12

--- Trimestre1/Uni3_Pra4/ej2/Dos_ruedas.php
+++ Trimestre1/Uni3_Pra4/ej2/Dos_ruedas.php
@@ -7,10 +7,18 @@
             parent::__construct($color, $peso);
             $this->cilindrada = $cilindrada;
         }
+        public function getCilindrada():float{
+            return $this->cilindrada;
+        }
+        public function setCilindrada(float $cilindrada){
+            $this->cilindrada=$cilindrada;
+        }
         public function poner_Gasolina($litros){
             parent::setPeso(parent::getPeso()+$litros);
         }
 
-        
+        public function _toString(){
+            return parent::_toString() . ", cilindrada: " . $this->getCilindrada();
+        }
     }
 ?>
