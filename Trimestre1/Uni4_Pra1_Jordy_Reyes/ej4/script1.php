a536779d0d85c7b04d908205335132ccf332d132
Jordy <jordyl.reyesl@gmail.com>
2024-10-23 10:08:14 +0000

    23 oct 2024, 12:08

--- Trimestre1/Uni4_Pra1_Jordy_Reyes/ej4/script1.php
+++ Trimestre1/Uni4_Pra1_Jordy_Reyes/ej4/script1.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="mes">Introduce un mes</label>
+        <input type="number" name="mes" id="mes">
+        <label for="anio">Introduce un anio</label>
+        <input type="number" name="anio" id="anio">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
 <?php
     if (isset($_POST['mes'])) {
         $diafinal=mktime(0,0,0,(int)$_POST['mes']+1,0,(int)$_POST['anio']);
@@ -30,25 +47,5 @@
             $i=$num;
         };
         echo "</table>";
-    }else{
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calendario</title>
-</head>
-<body>
-    <form action="#" method="post">
-        <label for="mes">Introduce un mes</label>
-        <input type="number" name="mes" id="mes">
-        <label for="anio">Introduce un anio</label>
-        <input type="number" name="anio" id="anio">
-        <input type="submit" value="Enviar">
-    </form>
-</body>
-</html>
-<?php
     }
 ?>
