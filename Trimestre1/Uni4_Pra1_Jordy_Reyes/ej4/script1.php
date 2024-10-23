691cd31d8b56cc691cad891a1fbfd9cdadd5a2ac
Jordy <jordyl.reyesl@gmail.com>
2024-10-23 10:40:24 +0000

    23 oct 2024, 12:40

--- Trimestre1/Uni4_Pra1_Jordy_Reyes/ej4/script1.php
+++ Trimestre1/Uni4_Pra1_Jordy_Reyes/ej4/script1.php
@@ -19,7 +19,6 @@
     if (isset($_POST['mes'])) {
         $diafinal=mktime(0,0,0,(int)$_POST['mes']+1,0,(int)$_POST['anio']);
         $diainicial=mktime(0,0,0,(int)$_POST['mes'],1,(int)$_POST['anio']);
-        echo date("Y-m-d H:i:s",$diafinal);
         echo<<<_END
             <table>
                 <tr>
@@ -36,7 +35,6 @@
                 </tr>
         _END;
         $inicio=date("N",$diainicial);
-        echo $inicio;
         $k=1;
         $num=0;
         for($i=1;$i<=date('j',$diafinal);$i++){
