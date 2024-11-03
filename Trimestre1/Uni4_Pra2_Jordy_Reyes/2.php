9edc1107d529eda03e3d7086a5c293b552e1644a
Joravs <jordyl.reyesl@gmail.com>
2024-11-03 14:11:02 +0000

    Nov 3, 2024, 3:11 PM

--- Trimestre1/Uni4_Pra2_Jordy_Reyes/2.php
+++ Trimestre1/Uni4_Pra2_Jordy_Reyes/2.php
@@ -1,3 +1,10 @@
 <?php
-    
+    function test_entrada($valor){
+        $valor=trim($valor);
+        $valor=stripslashes($valor);
+        return $valor;
+    }
+
+    $valor=" Es tu nombre O\'reilly? ";
+    echo test_entrada($valor);
 ?>
