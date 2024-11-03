9d8a9fa549762a35d28d3a437dceb009ab15d355
Joravs <jordyl.reyesl@gmail.com>
2024-11-03 14:32:38 +0000

    Nov 3, 2024, 3:32 PM

--- Trimestre1/Uni4_Pra2_Jordy_Reyes/funcion_validar_url.php
+++ Trimestre1/Uni4_Pra2_Jordy_Reyes/funcion_validar_url.php
@@ -1,6 +1,13 @@
 <?php
     function validar_url($value) {
         $urlErr="URL correcta";
-        if (empty($value))
+        if (empty($value)){
+            $urlErr="URL vacia";
+        }else{
+            if (!filter_var($value, FILTER_VALIDATE_URL)){
+                $urlErr="URL incorrecta";
+            }
+        }
+        return $urlErr;
     }
 ?>
