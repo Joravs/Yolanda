c6cbe5165f7a052b287d26f8740dd66f73c9e0eb
Joravs <jordyl.reyesl@gmail.com>
2024-11-03 14:17:33 +0000

    Nov 3, 2024, 3:17 PM

--- Trimestre1/Uni4_Pra2_Jordy_Reyes/practica.php
+++ Trimestre1/Uni4_Pra2_Jordy_Reyes/practica.php
@@ -10,3 +10,26 @@
 
 <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>"><!--Devuelve la misma url de la pagina-->
 
+<input type="radio" name="sexo"
+<?php if (isset($sexo) && $sexo=="mujer") echo "checked";?>
+value="mujer"> Mujer
+<input type="radio" name="sexo"
+<?php if (isset($sexo) && $sexo=="hombre") echo "checked";?>
+value="hombre"> Hombre
+<span class="error">* <?php //echo $sexoErr;?></span><br><br>
+
+
+<?php
+$email="abc@abc.com";
+$emailErr="Email correcto";
+if (empty($email)) {
+ $emailErr = "Se requiere Email";
+ } else {
+ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ $emailErr = "Fomato de Email invalido";
+ }
+ }
+echo $email;
+echo "<br>";
+echo $emailErr;
+?>
