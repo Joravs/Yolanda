a7f7b42a5803957050ee919a37c7d9572d32c707
Joravs <jordyl.reyesl@gmail.com>
2024-11-03 14:35:39 +0000

    Nov 3, 2024, 3:35 PM

--- Trimestre1/Uni4_Pra2_Jordy_Reyes/formulario.php
+++ Trimestre1/Uni4_Pra2_Jordy_Reyes/formulario.php
@@ -10,7 +10,12 @@
         <label for="name">Nombre:</label>
         <input type="text" id="name" name="name" required><br><br>
         <label for="email">Email:</label>
-        <input type="text" name="email" id="email">
+        <input type="text" name="email" id="email" required><br><br>
+        <label for="sexo">Sexo:</label>
+        <input type="radio" id="male" name="sexo" value="male">
+        <label for="male">Hombre</label>
+        <input type="radio" id="female" name="sexo" value="female">
+        <label for="female">Mujer</label><br><br>
     </form>
 </body>
 </html>
