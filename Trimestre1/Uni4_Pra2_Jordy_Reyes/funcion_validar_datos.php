631c681def03efdd75024545a254e3dfb4cc2a86
Joravs <jordyl.reyesl@gmail.com>
2024-11-03 15:25:11 +0000

    Nov 3, 2024, 4:25 PM

--- Trimestre1/Uni4_Pra2_Jordy_Reyes/funcion_validar_datos.php
+++ Trimestre1/Uni4_Pra2_Jordy_Reyes/funcion_validar_datos.php
@@ -1,7 +1,7 @@
 <?php
     function fieldreq(){
         if (isset($_POST['enviar'])){
-            if (empty($_POST['name']) || empty($_POST['url']) || empty($_POST['email'])){
+            if (empty($_POST['name']) || empty($_POST['url']) || empty($_POST['sexo']) || empty($_POST['email'])){
                 echo '* Required Fields';
             }
         }
