b51010f273cda0da0b5dff66b8833efc0306a9a7
Joravs <jordyl.reyesl@gmail.com>
2024-11-03 14:09:40 +0000

    Nov 3, 2024, 3:09 PM

--- Trimestre1/Uni4_Pra2_Jordy_Reyes/1.php
+++ Trimestre1/Uni4_Pra2_Jordy_Reyes/1.php
@@ -1,8 +1,7 @@
 <?php
     $valor=" Es tu nombre O\'reilly? ";
 
-    $resultado = trim($valor);
-    $resultado2= stripslashes($valor);
+    $resultado = trim($valor); // Elimina espacios del principio y final del valor
+    $resultado2= stripslashes($valor); // Elimina barras del valor
     echo $resultado . "<br>" . $resultado2;
-
 ?>
