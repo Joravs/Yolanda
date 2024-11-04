76355a18dd3bc2ef729e7e11cc6abbe4ff31355c
Jordy <jordyl.reyesl@gmail.com>
2024-11-04 13:13:43 +0000

    Nov 4, 2024, 2:13 PM

---
+++ Trimestre1/prueba/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simon Dice</title>
+</head>
+<body>
+    Generar una combiacion de 1 color: <br>
+    <button style="height:100px; border-radius: 50%; width: 100px;"></button>
+</body>
+</html>
