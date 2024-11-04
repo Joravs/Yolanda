6da939b9c040bce7caee2641f020f0c8ae451a02
Jordy <jordyl.reyesl@gmail.com>
2024-11-04 11:53:22 +0000

    Nov 4, 2024, 12:53 PM

--- Trimestre1/Uni4_Pra3_Jordy_Reyes/script.php
+++ Trimestre1/Uni4_Pra3_Jordy_Reyes/script.php
@@ -1,3 +1,11 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficheros</title>
+</head>
+<body>
+    <form action="upload.php" method="post"></form>
+</body>
+</html>
