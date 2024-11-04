35ae77ae9ecac8663adef550d6a648f2cbda1229
Jordy <jordyl.reyesl@gmail.com>
2024-11-04 12:45:13 +0000

    Nov 4, 2024, 1:45 PM

--- Trimestre1/Uni4_Pra4_Jordy_Reyes/cookies.php
+++ Trimestre1/Uni4_Pra4_Jordy_Reyes/cookies.php
@@ -1,3 +1,24 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post">
+        Seleccione de que color desea que sea la web de ahora en adelante:
+        <input type="radio" name="color" value="Verde">
+        <input type="radio" name="color" value="Rojo">
+        <input type="radio" name="color" value="Azul">
+        <input type="submit" value="Crear Cookie" name="enviar">
+    </form>
+    <p>Se crea la cookie</p>
+    <a href="<?php echo $_SERVER["PHP_SELF"]?>">Ir a la otra web</a>
+    <?php
+        if(isset($_POST["enviar"])){
+            
+        }
+    ?>
+</body>
+</html>
