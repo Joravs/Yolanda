ce6a8d09bf43f563401745207823f618c0fcad94
Jordy <jordyl.reyesl@gmail.com>
2024-11-04 12:58:49 +0000

    Nov 4, 2024, 1:58 PM

--- Trimestre1/Uni4_Pra4_Jordy_Reyes/cookies.php
+++ Trimestre1/Uni4_Pra4_Jordy_Reyes/cookies.php
@@ -1,24 +1,31 @@
+<?php
+    if(isset($_COOKIE['color'])){ $style = $_COOKIE['color'];}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookies</title>
+    <style>
+        body{
+            background-color: <?php echo $style?>;
+        }
+    </style>
 </head>
 <body>
-    <form action="cookieaplicada.php" method="post">
-        Seleccione de que color desea que sea la web de ahora en adelante:
-        <input type="radio" name="color" value="Verde">
-        <input type="radio" name="color" value="Rojo">
-        <input type="radio" name="color" value="Azul">
+    <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post">
+        Seleccione de que color desea que sea la web de ahora en adelante: <br><br>
+        <label for="green">Verde</label>
+        <input type="radio" id='green' name="color" value="green"><br>
+        <label for="rojo">Rojo</label>
+        <input type="radio" id='rojo' name="color" value="red"><br>
+        <label for="azul">Azul</label>
+        <input type="radio" id='azul' name="color" value="blue"><br><br>
         <input type="submit" value="Crear Cookie" name="enviar">
         <?php
-            if(isset($_POST["enviar"])){
-                setcookie("color", $_POST["color"]);
-            }
+            setcookie("color", $_POST["color"]);
         ?>
     </form>
-    <p>Se crea la cookie</p>
-    <a href="<?php echo $_SERVER["PHP_SELF"]?>">Ir a la otra web</a>
 </body>
 </html>
