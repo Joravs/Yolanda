d5d71b2f2a32aac376bc11290f9f807e22a10cb5
Jordy <jordyl.reyesl@gmail.com>
2024-11-05 10:01:03 +0000

    Nov 5, 2024, 11:01 AM

--- Trimestre1/prueba/adivina.php
+++ Trimestre1/prueba/adivina.php
@@ -9,13 +9,17 @@
 <body>
 <form action="verificar.php" method='post'>
     Generar una combiacion de 1 color: <br>
+    <script>function cambiarColor(id){
+
+    }
+    </script>
     <?php
         $colors = ["red", "blue", "green", "yellow"];
         $color="black";
         for($i=0;$i<4;$i++){
             $_SESSION['color'.$i] = $_POST['color'.$i];
-            echo '<button style="height:100px; border-radius: 50%; width: 100px; background-color:'. $color.';"></button>';
-            echo '<button onclick="">'.$colors[$i].'</button>';
+            echo '<button id="b'.$i.'" style="height:100px; border-radius: 50%; width: 100px; background-color:'. $color.';"></button>';
+            echo '<button onclick="cambiarColor("b'.$i.'")">'.$colors[$i].'</button>';
         }
     ?>
     <input type="submit" value="Comprobar">
