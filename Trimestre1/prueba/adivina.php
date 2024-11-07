7685482736bd9a901bcede4e2d513e15abcfc2a5
Jordy <jordyl.reyesl@gmail.com>
2024-11-07 08:31:37 +0000

    Nov 7, 2024, 9:31 AM

--- Trimestre1/prueba/adivina.php
+++ Trimestre1/prueba/adivina.php
@@ -15,11 +15,14 @@
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $html = curl_exec($ch);
             curl_close($ch);
+            $dom = new DOMDocument();
+            @$dom->loadHTML($html);
+            $xpath = new DOMXPath($dom);
+            $elements = $xpath->query($selector);
             function cambiarColor($btn){
                 $color='black';
             
             }
-            echo '127.0.0.1'.$_SERVER['PHP_SELF'];
             $color="black";
             for($i=0;$i<4;$i++){
                 $_SESSION['color'.$i] = $_POST['color'.$i];
