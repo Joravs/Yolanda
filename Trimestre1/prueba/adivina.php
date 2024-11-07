f9a37d886c6224515efed30b711b63829926afd6
Jordy <jordyl.reyesl@gmail.com>
2024-11-07 08:37:45 +0000

    Nov 7, 2024, 9:37 AM

--- Trimestre1/prueba/adivina.php
+++ Trimestre1/prueba/adivina.php
@@ -10,29 +10,30 @@
     <form action="verificar.php" method='post'>
         Generar una combiacion de 1 color: <br>
         <?php
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $_SERVER['PHP_SELF']);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $html = curl_exec($ch);
-            curl_close($ch);
-            $dom = new DOMDocument();
-            @$dom->loadHTML($html);
-            $xpath = new DOMXPath($dom);
-            $elements = $xpath->query("//p[@]");
-
             function cambiarColor($btn){
-                $color='black';
-            
-            }
-            $color="black";
-            for($i=0;$i<4;$i++){
-                $_SESSION['color'.$i] = $_POST['color'.$i];
-                echo '<p id="id" style="height:100px; border-radius: 50%; width: 100px; background-color: black;"></p>';
-                for($j=0;$j<count($colors);$j++){
-                    echo '<button onclick="'. cambiarColor("p.id".$i).'"type="button" id="boton'.$j.'" value="'.$colors[$j].'">'.$colors[$j].'</button>';
+                $color="black";
+                
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $_SERVER['PHP_SELF']);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $html = curl_exec($ch);
+                curl_close($ch);
+                
+                $dom = new DOMDocument();
+                @$dom->loadHTML($html);
+                $xpath = new DOMXPath($dom);
+                $elements = $xpath->query("//p[@$btn]");
+
+                for($i=0;$i<4;$i++){
+                    $_SESSION['color'.$i] = $_POST['color'.$i];
+                    echo '<p id="id" style="height:100px; border-radius: 50%; width: 100px; background-color: black;"></p>';
+                    for($j=0;$j<count($colors);$j++){
+                        echo '<button onclick="'. cambiarColor("p.id".$i).'"type="button" id="boton'.$j.'" value="'.$colors[$j].'">'.$colors[$j].'</button>';
+                    }
+                    echo '<br>';
                 }
-                echo '<br>';
             }
+            cambiarColor('id0');
         ?>
         <input type="submit" value="Comprobar">
     </form>
