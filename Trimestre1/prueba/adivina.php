2ad58fe471cace14f51d5d96ec93c37afc4722df
Jordy <jordyl.reyesl@gmail.com>
2024-11-07 08:06:39 +0000

    Nov 7, 2024, 9:06 AM

--- Trimestre1/prueba/adivina.php
+++ Trimestre1/prueba/adivina.php
@@ -9,21 +9,16 @@
 <body>
     <form action="verificar.php" method='post'>
         Generar una combiacion de 1 color: <br>
-        <script>
-        function cambiarColor(id,btn){
-            let circulo = document.getElementById(id);
-            const boton = document.getElementById(btn);
-            circulo.style.backgroundColor = boton.innerHTML;
-            console.log("boton: " + boton.innerHTML);
-        }
-        </script>
         <?php
+            function cambiarColor($id, $btn){
+                
+            }
             $color="black";
             for($i=0;$i<4;$i++){
                 $_SESSION['color'.$i] = $_POST['color'.$i];
-                echo '<button id="b'.$i.'" style="height:100px; border-radius: 50%; width: 100px; background-color:'. $color.';"></button>';
+                echo '<p id="b'.$i.'" style="height:100px; border-radius: 50%; width: 100px; background-color:'. $color.';"></p>';
                 for($j=0;$j<count($colors);$j++){
-                    echo '<button type="button" id="boton'.$j.'" onclick="cambiarColor("b'.$i.'","boton'.$j.'")" value="'.$colors[$j].'">'.$colors[$j].'</button>';
+                    echo '<button type="button" id="boton'.$j.'" onclick="'.cambiarColor("b".$i,"boton".$j).'" value="'.$colors[$j].'">'.$colors[$j].'</button>';
                 }
                 echo '<br>';
             }
