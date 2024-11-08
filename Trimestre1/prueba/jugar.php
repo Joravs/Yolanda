f97854e478dad1689d68dbce92f0a1c42d074132
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 17:45:33 +0000

    Nov 8, 2024, 6:45 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -37,6 +37,7 @@
         <form action="<?php if($_SESSION['contador']==4){
             $acierto=0;
             for ($i=1;$i<=4;$i++){
+                echo "circulo $i: ".$_SESSION['circulo'.$i]." respuesta: ".$_SESSION['resp'.$i]."<br>";
                 if ($_SESSION['circulo'.$i]==$_SESSION['resp'.$i]){
                     $acierto++;
                 }
