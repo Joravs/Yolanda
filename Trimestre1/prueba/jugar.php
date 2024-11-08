b39cb9bc1a760d43d740994eff8bcd681d0852d4
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 18:37:42 +0000

    Nov 8, 2024, 7:37 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -1,6 +1,21 @@
 <?php
     session_start();
     include "pintar-circulos.php";
+    function actionForm(){
+        if($_SESSION['contador']==4){
+            $acierto=0;
+            for ($i=1;$i<=4;$i++){
+                if ($_SESSION['circulo'.$i]==$_SESSION['resp'.$i]){
+                    $acierto++;
+                }
+            }
+            if ($acierto==4){
+                echo "acierto.php";
+            }else{
+                echo "fallo.php";
+            }
+        }else{echo $_SERVER['PHP_SELF'];} 
+    }
     if (isset($_POST['jugar'])){
         $_SESSION['contador']=0;
         $color="black";
@@ -36,20 +51,7 @@
     </head>
     <body>
         <h1>Adivina</h1>
-        <form action="<?php 
-        if($_SESSION['contador']==4){
-            $acierto=0;
-            for ($i=1;$i<=4;$i++){
-                if ($_SESSION['circulo'.$i]==$_SESSION['resp'.$i]){
-                    $acierto++;
-                }
-            }
-            if ($acierto==4){
-                echo "acierto.php";
-            }else{
-                echo "fallo.php";
-            }
-        }else{echo $_SERVER['PHP_SELF'];} ?>" method="post">
+        <form action="<?php actionForm()?>" method="post">
 <?php              
             pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);
         }
