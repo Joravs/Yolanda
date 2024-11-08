6dba4743be3fc6de197cb9558ce2dac39075f6e7
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 16:32:30 +0000

    Nov 8, 2024, 5:32 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
-    require_once "pintar-circulos.php";
+    require_once "pintar-circulos.php";   
     if (isset($_POST['enviar'])){
         for($i=1;$i<=count($_POST)-1;$i++){
             $_SESSION['circulo'.$i]=$_POST['circulo'.$i];
         }
-        pintarCirculos();
+        $colors=["black","black","black","black"];
+        pintar_circulos($colors[0],$colors[1],$colors[2],$colors[3]);
     }
 ?>
