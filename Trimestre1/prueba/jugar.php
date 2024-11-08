188f21e02285fa16df211eb603b14cb4ad73ec8f
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 17:04:23 +0000

    Nov 8, 2024, 6:04 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -19,21 +19,14 @@
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
             <?php
             pintar_circulos($color,$color,$color,$color);
+        }else{
+                
         }
         ?>
-        <?php
-            $color="black";
-            $i=1;
-            if(isset($_POST["color1"])){
-                $_SESSION['resp'.$i]=$_POST['color'.$i];
-                $i++;
-                pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);
-        }
-        ?>
-        <input type="submit" name="color1" value="red" style="background-color: red">
-        <input type="submit" name="color2" value="yellow" style="background-color: yellow">
-        <input type="submit" name="color3" value="green" style="background-color: green">
-        <input type="submit" name="color4" value="blue" style="background-color: blue">
+        <button type="submit" name="color1" value="red" style="background-color: red">Rojo</button>
+        <button type="submit" name="color2" value="yellow" style="background-color: yellow">Amarillo</button>
+        <button type="submit" name="color3" value="green" style="background-color: green">Verde</button>
+        <button type="submit" name="color4" value="blue" style="background-color: blue">Azul</button>
     </form>
 </body>
 </html>
