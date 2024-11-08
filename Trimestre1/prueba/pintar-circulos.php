e7a82b636f1991427396adb17325fc59c358ceb0
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 16:25:11 +0000

    Nov 8, 2024, 5:25 PM

--- Trimestre1/prueba/pintar-circulos.php
+++ Trimestre1/prueba/pintar-circulos.php
@@ -1,9 +1,9 @@
 <?php
     function pintar_cirulos($c1,$c2,$c3,$c4){
-        echo '<input class="circulo" style="background-color:'.$c1.';" value="'.$c1.'"></input>';
-        echo '<input class="circulo" style="background-color:'.$c2.';" value="'.$c2.'"></input>';
-        echo '<input class="circulo" style="background-color:'.$c3.';" value="'.$c3.'"></input>';
-        echo '<input class="circulo" style="background-color:'.$c4.';" value="'.$c4.'"></input>';
+        echo '<input class="circulo" style="background-color:'.$c1.';" name="circulo1" value="'.$c1.'"></input>';
+        echo '<input class="circulo" style="background-color:'.$c2.';" name="circulo2" value="'.$c2.'"></input>';
+        echo '<input class="circulo" style="background-color:'.$c3.';" name="circulo3" value="'.$c3.'"></input>';
+        echo '<input class="circulo" style="background-color:'.$c4.';" name="circulo4" value="'.$c4.'"></input>';
     }
 ?>
 <!DOCTYPE html>
