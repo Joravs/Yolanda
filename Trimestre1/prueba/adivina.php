8a835e642e6763aa3a0c1e32b120ac53044f1658
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 15:50:37 +0000

    Nov 8, 2024, 4:50 PM

--- Trimestre1/prueba/adivina.php
+++ Trimestre1/prueba/adivina.php
@@ -7,6 +7,7 @@
     <title>Simon Dice</title>
 </head>
 <body>
+    <form action="verificar.php" method="post">
         Adivinar una combiacion de 4 color: <br>
     <?php
             $colors=["red", "blue", "green", "yellow"];
@@ -17,21 +18,18 @@
                 }
                 return $color;
             }
-        echo '<form action="verificar.php" method="post">';
             for($i=0;$i<4;$i++){
                 $_SESSION['color'.$i] = $_POST['color'.$i];
-                echo '<div>';
-                echo "<form method='get' action=".$_SERVER["PHP_SELF"].">";
+                echo "<div role='form' method='get' action=".$_SERVER["PHP_SELF"].">";
                 echo '<p id="id'.$i.'" style="height:100px; border-radius: 50%; width: 100px; background-color:'. cambiarColor("id".$i).'"></p>';
                 for($j=0;$j<count($colors);$j++){
                     echo '<button name="boton'.$i.'" value="'.$colors[$j].'">'.$colors[$j].'</button>';
                 }
-                echo "</form>";
                 echo "</div>";
                 echo '<br>';
             }
-        echo '</form>';
-    ?>
+            ?>
         <input type="submit" value="Comprobar">
+        </form>
 </body>
 </html>
