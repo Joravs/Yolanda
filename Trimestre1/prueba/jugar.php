54a0fec2356da986288c90df249e1ee6fb041951
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 16:49:22 +0000

    Nov 8, 2024, 5:49 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -21,8 +21,12 @@
             pintar_circulos($color,$color,$color,$color);
         }
         ?>
+        <input type="submit" name="color" value="red" style="background-color: red">
+        <input type="submit" name="color" value="yellow" style="background-color: yellow">
+        <input type="submit" name="color" value="green" style="background-color: green">
+        <input type="submit" name="color" value="blue" style="background-color: blue">
         <?php
-        else{
+            else{
             for($i=1;$i<=4;$i++){
                 $_SESSION['resp'.$i]=$_GET['color'];
                 pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);
