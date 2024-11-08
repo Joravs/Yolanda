22c88640b25ddb631ec3c1e4be80d6039ec89f49
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 16:11:53 +0000

    Nov 8, 2024, 5:11 PM

--- Trimestre1/prueba/pintar-circulos.php
+++ Trimestre1/prueba/pintar-circulos.php
@@ -1,6 +1,6 @@
 <?php
     function pintar_cirulos($c1,$c2,$c3,$c4){
-
+        <button class="circulo" disabled="disabled"></button>
     }
 ?>
 <!DOCTYPE html>
@@ -9,10 +9,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simon</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="jugar.php" method="post">
-        
+
     </form>
 </body>
 </html>
