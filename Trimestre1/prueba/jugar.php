bda308a7a7fd26272e6d97a75b84a4076d500910
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 16:55:03 +0000

    Nov 8, 2024, 5:55 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -25,15 +25,15 @@
             $color="black";
             if(isset($_GET["color"])){
                 for($i=1;$i<=4;$i++){
-                $_SESSION['resp'.$i]=$_GET['color'];
+                $_SESSION['resp'.$i]=$_GET['color'.$i];
                 pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);
             }
         }
         ?>
-        <input type="submit" name="color" value="red" style="background-color: red">
-        <input type="submit" name="color" value="yellow" style="background-color: yellow">
-        <input type="submit" name="color" value="green" style="background-color: green">
-        <input type="submit" name="color" value="blue" style="background-color: blue">
+        <input type="submit" name="color1" value="red" style="background-color: red">
+        <input type="submit" name="color2" value="yellow" style="background-color: yellow">
+        <input type="submit" name="color3" value="green" style="background-color: green">
+        <input type="submit" name="color4" value="blue" style="background-color: blue">
     </form>
 </body>
 </html>
