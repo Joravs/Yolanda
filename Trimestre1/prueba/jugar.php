732b6047c64fb2a0b26f8e4215d7e32b56495c5c
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 18:12:29 +0000

    Nov 8, 2024, 7:12 PM

--- Trimestre1/prueba/jugar.php
+++ Trimestre1/prueba/jugar.php
@@ -49,14 +49,15 @@
             }
         }else{echo $_SERVER['PHP_SELF'];} ?>" method="post">
 <?php              
-            pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);    
-            $_SESSION['contador']++;
-        }
+            pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);    }
         ?>
         <button type="submit" name="color" value="red" style="background-color: red">Rojo</button>
         <button type="submit" name="color" value="yellow" style="background-color: yellow">Amarillo</button>
         <button type="submit" name="color" value="green" style="background-color: green">Verde</button>
         <button type="submit" name="color" value="blue" style="background-color: blue">Azul</button>
+        <?php
+            $_SESSION['contador']++;
+        ?>
     </form>
 </body>
 </html>
