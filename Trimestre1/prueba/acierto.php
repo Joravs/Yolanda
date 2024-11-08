642b9e79b68f8bc4ffa6c9c0e2e115e427e2d927
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 17:17:53 +0000

    Nov 8, 2024, 6:17 PM

---
+++ Trimestre1/prueba/acierto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acierto</title>
+</head>
+<body>
+    <h1>Simon</h1>
+    <?php
+        if($_SESSION['contador'])
+    ?>
+</body>
+</html>
