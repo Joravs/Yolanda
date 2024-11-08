f4f0767eaaee5cc7c462386a6b3f1cb892ac92f5
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 17:25:26 +0000

    Nov 8, 2024, 6:25 PM

--- Trimestre1/prueba/acierto.php
+++ Trimestre1/prueba/acierto.php
@@ -8,9 +8,11 @@
 </head>
 <body>
     <h1>Simon</h1>
+    <h2>Has acertado todos los circulos</h2>
     <?php
         include "pintar-circulos.php";
-
+        pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);
     ?>
+    <a href="index.php">Volver a jugar</a>
 </body>
 </html>
