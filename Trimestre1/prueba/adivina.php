d375e624488ffe9c07336790941f96c3223566f6
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 15:34:59 +0000

    Nov 8, 2024, 4:34 PM

--- Trimestre1/prueba/adivina.php
+++ Trimestre1/prueba/adivina.php
@@ -11,13 +11,13 @@
         Generar una combiacion de 1 color: <br>
         <?php
             $colors=["red", "blue", "green", "yellow"];
-            function cambiarColor($btn){ 
+            function cambiarColor(){ 
                 $color="black";
             }
             for($i=0;$i<4;$i++){
                 $_SESSION['color'.$i] = $_POST['color'.$i];
                 echo "<form method='get'>";
-                echo '<p id="id'.$i.'" style="height:100px; border-radius: 50%; width: 100px; background-color:'. cambiarColor;.'"></p>';
+                echo '<p id="id'.$i.'" style="height:100px; border-radius: 50%; width: 100px; background-color:'. cambiarColor().'"></p>';
                 for($j=0;$j<count($colors);$j++){
                     echo '<button id="boton'.$j.'" value="'.$colors[$j].'">'.$colors[$j].'</button>';
                 }
