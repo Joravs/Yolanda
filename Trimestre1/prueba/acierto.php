fc6b63f7d01cca9d40bad6ffabdd1779726494ad
Joravs <jordyl.reyesl@gmail.com>
2024-11-08 17:19:59 +0000

    Nov 8, 2024, 6:19 PM

--- Trimestre1/prueba/acierto.php
+++ Trimestre1/prueba/acierto.php
@@ -8,7 +8,15 @@
 <body>
     <h1>Simon</h1>
     <?php
-        if($_SESSION['contador'])
+        $acierto=0;
+        for ($i=1;$i<=4;$i++){
+            if ($_SESSION['circulo'.$i]==$_SESSION['resp'.$i]){
+                $acierto++;
+            }
+        }
+        if ($acierto==4){
+            echo "<h2>Has acertado todos los colores</h2>";
+        }
     ?>
 </body>
 </html>
