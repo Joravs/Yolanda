eb4e8a3e3049d14c7df9bd902686fa84b1049b35
Jordy <jordyl.reyesl@gmail.com>
2024-11-14 12:55:50 +0000

    Nov 14, 2024, 1:55 PM

--- Trimestre1/PruebaAutoEvaluable/ejercicio3/ejercicio3.php
+++ Trimestre1/PruebaAutoEvaluable/ejercicio3/ejercicio3.php
@@ -15,7 +15,6 @@
         return $resp;
     }
     ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,33 +23,35 @@
     <title>Ejercicio 3</title>
 </head> 
 <body>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
 <?php
     if(!isset($_POST['enviar'])){
         echo $_SESSION['numRand']=generarNumero();
         $_SESSION['cont']=0;
 ?>
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
         <label for="num">Adivina mi numero</label>
         <input type="number" name="num" id="num">
         <input type="submit" value="Enviar" name="enviar">
-    </form>
 <?php
     }else{
-    $numeroes=mayorMenor($_SESSION['numRand'],$_POST['num']);
-    echo "<h3>Tu numero es:".$_POST['num']."</h3>";
-    switch($numeroes){
-        case "Mayor":
-            case "Menor":
-                echo "<h3>Mi numero es ".$numeroes."</h3>";
-                $_SESSION['cont']++;
-                echo '<a href="'.$_SERVER['PHP_SELF'].'">Sigue jugando...</a>';
-                break;
-                case "True":
-                    echo "<h2>ENHORABUENA, HAS ACERTADO</h2>";
-                    echo '<h3>Has necesitado '.$_SESSION['cont'].' intentos</h3>';
-                    session_destroy();
+        $_SESSION['num']=$_POST['num'];
+        $numeroes=mayorMenor($_SESSION['numRand'],$_SESSION['num']);
+        echo "<h3>Tu numero es:".$_POST['num']."</h3>";
+        switch($numeroes){
+            case "Mayor":
+                case "Menor":
+                    echo "<h3>Mi numero es ".$numeroes."</h3>";
+                    $_SESSION['cont']++;
+                    break;
+                    case "True":
+                        echo "<h2>ENHORABUENA, HAS ACERTADO</h2>";
+                        echo '<h3>Has necesitado '.$_SESSION['cont'].' intentos</h3>';
+                        session_destroy();
+                    }
                 }
-            }
 ?>
+                <br>
+<a href="<?php echo $_SERVER['PHP_SELF']?>">Sigue jugando...</a>
+</form>
 </body>
 </html>
