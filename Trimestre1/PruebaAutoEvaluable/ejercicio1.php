3a65a6b23f1786ffb2554e2a7ca8b23e32858ca2
Jordy <jordyl.reyesl@gmail.com>
2024-11-14 11:11:24 +0000

    Nov 14, 2024, 12:11 PM

--- Trimestre1/PruebaAutoEvaluable/ejercicio1.php
+++ Trimestre1/PruebaAutoEvaluable/ejercicio1.php
@@ -6,24 +6,24 @@
     <title>Ejercicio 1</title>
 </head>
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"></form>
-    <?php
-    $matriz=[];
-        for($i=0;$i<3;$i++){
-            for($j=0;$j<2;$j++){
-                echo "<label for='$i$j'>E.$i.$j</label>";
-                echo "<input id='$i$j'type='number' name='$i$j'></input>";
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+        <?php
+        $matriz=[];
+            for($i=0;$i<3;$i++){
+                for($j=0;$j<2;$j++){
+                    echo "<label for='$i$j'>E.$i.$j</label>";
+                    echo "<input id='$i$j'type='number' name='$i$j'></input>";
+                }
+                echo "<br>";
             }
-            echo "<br>";
-        }
-    ?>
-    <input type="submit" value="Calcular" name="calcular">
+        ?>
+        <input type="submit" value="Calcular" name="calcular">
+    </form>
     <?php
         if(isset($_POST['calcular'])){
             for($i=0;$i<3;$i++){
                 for($j=0;$j<2;$j++){
                     echo "<p>".$_POST[$i.$j]." = ".decbin($_POST[$i.$j])."</p>";
-                    echo "<br>";
                 }
             }
         }
