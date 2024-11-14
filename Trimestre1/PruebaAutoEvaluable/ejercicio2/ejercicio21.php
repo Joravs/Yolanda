a772deddae977dfc10b8743bcea5ca418f555d1a
Jordy <jordyl.reyesl@gmail.com>
2024-11-14 11:13:46 +0000

    Nov 14, 2024, 12:13 PM

--- Trimestre1/PruebaAutoEvaluable/ejercicio2/ejercicio21.php
+++ Trimestre1/PruebaAutoEvaluable/ejercicio2/ejercicio21.php
@@ -1,11 +1,17 @@
+<?php
+    function generarBinario(){
+        
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio 2</title>
 </head>
 <body>
-    
+    <h1>Adivina el numero en decimal</h1>
+    <h2>El numero en BINARIO</h2>
 </body>
 </html>
