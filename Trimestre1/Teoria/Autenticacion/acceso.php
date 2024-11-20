2be8ebcaa64bf1e539c12090db81c48eea32d065
Jordy <jordyl.reyesl@gmail.com>
2024-11-20 12:51:55 +0000

    Nov 20, 2024, 1:51 PM

---
+++ Trimestre1/Teoria/Autenticacion/acceso.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <form method="post" action="prueba.php">
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" name="name"><br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password">
+        <input type="submit" value="Enviar" name="enviar">
+    </form>
+</body>
+</html>
---
+++ Trimestre1/Teoria/Autenticacion/prueba.php
@@ -0,0 +1,10 @@
+<?php
+    $usu="pepito";
+    $pass="123";
+
+    if(isset($_POST['enviar'])){
+        if($_POST['name'] == $usu && $_POST['password'] == $pass){
+            echo "Te has logeado";
+        }
+    }
+?>
