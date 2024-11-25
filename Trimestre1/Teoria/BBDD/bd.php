d5141fa4c215bff54fcef552821bb11b6a19ae31
Jordy <jordyl.reyesl@gmail.com>
2024-11-25 12:42:24 +0000

    Nov 25, 2024, 1:42 PM

--- Trimestre1/Teoria/BBDD/bd.php
+++ Trimestre1/Teoria/BBDD/bd.php
@@ -6,6 +6,10 @@
     $ctbd = new mysqli($hn,$u,$pw,$db);
     $query="Select usu,contra from usuarios";
     $result=$ctbd->query($query);
-    if($ctbd->connect_error){die ("Fatal error: ".$ctbd->connect_error);}
-    else{if(!$result){die("Fatal error: ".$resul);}else{echo htmlspecialchars($result->fetch_assoc()['usu']);}}
+    if($ctbd->connect_error)die ("Fatal error: ".$ctbd->connect_error);
+    $rows=$result->num_rows;
+    for($i=0;$i<$rows;$i++){
+        $result=$result->data_seek($i);
+        echo "Usuario ".$i+1 . htmlspecialchars($result->fetch_assoc()['usu']);
+    }
 ?>
