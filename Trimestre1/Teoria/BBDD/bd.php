5f5559935245e3fcc05e5389622f083fcad28764
Jordy <jordyl.reyesl@gmail.com>
2024-11-25 12:46:33 +0000

    Nov 25, 2024, 1:46 PM

--- Trimestre1/Teoria/BBDD/bd.php
+++ Trimestre1/Teoria/BBDD/bd.php
@@ -9,7 +9,7 @@
     if($ctbd->connect_error)die ("Fatal error: ".$ctbd->connect_error);
     $rows=$result->num_rows;
     for($i=0;$i<$rows;$i++){
-        $result=$result->data_seek($i);
-        echo "Usuario ".($i+1) . htmlspecialchars($result->fetch_assoc()['usu']);
+        $result->data_seek($i);
+        echo "Usuario: ". htmlspecialchars($result->fetch_assoc()['usu'])."<br>";
     }
 ?>
