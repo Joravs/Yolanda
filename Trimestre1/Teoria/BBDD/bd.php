1e4c4655eab9bd8f4af1d1a1d71b086cbe30a563
Jordy <jordyl.reyesl@gmail.com>
2024-11-25 12:18:25 +0000

    Nov 25, 2024, 1:18 PM

--- Trimestre1/Teoria/BBDD/bd.php
+++ Trimestre1/Teoria/BBDD/bd.php
@@ -1,3 +1,10 @@
 <?php
-        
+    $hn = 'localhost';
+    $db = 'bdsimon';    
+    $u = 'jugador';
+    $pw ='';
+
+    $ctbd = new mysqli($hn,$u,$pw,$db);
+    $salida=$ctbd->connect_error?"Fatal error: " . $ctbd->connect_error:"Coneection successfully connected";
+    echo $salida;
 ?>
