ae537f16d175056a031e3c37883aa3393a945963
Jordy <jordyl.reyesl@gmail.com>
2024-11-28 12:11:37 +0000

    Nov 28, 2024, 1:11 PM

--- Trimestre1/Teoria/BBDD/FormularioBBDD/scripts/registro.php
+++ Trimestre1/Teoria/BBDD/FormularioBBDD/scripts/registro.php
@@ -1,9 +1,12 @@
 <?php
+    session_start();
+    $_SESSION['incorrect']=0;
     function validarContra(){
         if(isset($_POST['registro'])){
             if($_POST['passwordreg']==$_POST['passwordreg2']){
                 echo 'acceso.php';
             }else{
+                $_SESSION['incorrect']=1;
                 echo $_SERVER['PHP_SELF'];
             }
         }
@@ -19,6 +22,7 @@
 <body>
     <h1>Registro</h1>
     <form action="<?php validarContra();?>" method="post">
+        <?php if($_SESSION['incorrect']==1){echo 'Contraseñas no coinciden';}?>
         <label for="usuarioreg">Usuario: </label>
         <input type="text" name="usuarioreg" id="usuarioreg">
         <label for="passwordreg">Contraseña: </label>
