f519d3b19e6c7e17ecb2b46ba406b15f8bffb616
Jordy <jordyl.reyesl@gmail.com>
2024-11-28 13:15:29 +0000

    Nov 28, 2024, 2:15 PM

--- Trimestre1/Simon/jugar.php
+++ Trimestre1/Simon/jugar.php
@@ -2,7 +2,7 @@
     session_start();
     include "pintar-circulos.php";
     function actionForm(){
-        if($_SESSION['contador']==4){
+        if($_SESSION['contador']==3){
             $acierto=0;
             for ($i=1;$i<=4;$i++){
                 if ($_SESSION['circulo'.$i]==$_SESSION['resp'.$i]){
