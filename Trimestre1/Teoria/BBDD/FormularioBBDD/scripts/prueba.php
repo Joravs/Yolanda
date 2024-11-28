8f17abf97517d6a356d688f0873497827ad5c183
Jordy <jordyl.reyesl@gmail.com>
2024-11-28 11:57:31 +0000

    Nov 28, 2024, 12:57 PM

--- Trimestre1/Teoria/BBDD/FormularioBBDD/scripts/prueba.php
+++ Trimestre1/Teoria/BBDD/FormularioBBDD/scripts/prueba.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'datdb.php';
     $ctdb=new mysqli($hn,$user,$pw,$db);
+    $resultado="Usuario o contraseña incorrectos<br><a href='acceso.php'>Volver a Intentarlo</a>";
     if(isset($_POST['enviar'])){
         if($ctdb->connect_error){die("Connection Error");}
         else{
@@ -8,18 +9,18 @@
             $pass=$_POST['password'];
             $qury="Select usu,contra from usuarios where usu='$u' AND contra='$pass'";
             $result=$ctdb->query($qury);
-            if($result){}
-            else{
-                $uValido=htmlspecialchars($result->fetch_assoc()['usu']);
-                $result->data_seek(0);
-                $passValido=htmlspecialchars($result->fetch_assoc()['contra']);
+            if($result->num_rows==0){
+                echo $resultado;
+                return;
             }
+            $uValido=htmlspecialchars($result->fetch_assoc()['usu']);
+            $result->data_seek(0);
+            $passValido=htmlspecialchars($result->fetch_assoc()['contra']);
             if($uValido==$u && $passValido==$pass){
                 echo "Te has logeado";
                 echo '<a href="acceso.php">Vuelve al Inicio</a>';
             }else{
-                echo "Usuario o contraseña incorrectos<br>";
-                echo '<a href="acceso.php">Volver a Intentarlo</a>';
+                echo $resultado;
             }
         }
     }
