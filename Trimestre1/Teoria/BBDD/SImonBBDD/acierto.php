cc560acdd0eaa2bbf68de06d8a889094d93c3a8a
Joravs <jordyl.reyesl@gmail.com>
2024-12-02 17:25:35 +0000

    Dec 2, 2024, 6:25 PM

--- Trimestre1/Teoria/BBDD/SImonBBDD/acierto.php
+++ Trimestre1/Teoria/BBDD/SImonBBDD/acierto.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     require_once 'pintar-circulos.php';
+    $ur=$_SESSION['usuario'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,8 +12,16 @@
 </head>
 <body>
     <h1>SIMON</h1>
-    <h2><?php echo $_SESSION['usuario']; ?> enhorabuena, has acertado</h2>
+    <h2><?php echo $ur; ?> enhorabuena, has acertado</h2>
     <?php pintar_circulos($_SESSION['circulosValidos']);?>
+    <?php
+        require_once 'datdb.php';
+        $ctdb=new mysqli($hn,$user,$pw,$db);
+        if($ctdb->connect_error)die("Error connecting");
+        $qryInsert="INSERT INTO jugadas (codigousu,acierto) VALUES ((SELECT Codigo FROM usuarios where Nombre='ur'),1);";
+        $ctdb->query($qryInsert);
+        $ctdb->close();
+    ?>
     <p>Se ha guardado en la base de datos</p>
     <a href="inicio.php">Volver a jugar</a>
     <a href="estadisticas.php">Estadisticas</a>
