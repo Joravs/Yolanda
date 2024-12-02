e5960def42a14bbf02b3ae46a9f199099c1d2d9f
Jordy <jordyl.reyesl@gmail.com>
2024-12-02 13:11:43 +0000

    Dec 2, 2024, 2:11 PM

--- Trimestre1/Teoria/BBDD/SImonBBDD/jugar.php
+++ Trimestre1/Teoria/BBDD/SImonBBDD/jugar.php
@@ -7,6 +7,7 @@
             for($i=0;$i<count($colores);$i++){
                 $_SESSION['coloresPulsados'][$i]=$colorBlack;
             }
+            pintar_circulos($_SESSION['coloresPulsados']);
         }else{
             pintar_circulos($_SESSION['coloresPulsados']);
         }
@@ -23,5 +24,11 @@
     <h1>SIMON</h1>
     <h2><?php echo $_SESSION['usuario'];?> pulsa los botones en el orden correspondiente.</h2>
     <?php juegoPulsar();?>
+    <form action="post">
+        <button type="submit" name="red">Rojo</button>
+        <button type="submit" name="yellow">Amarillo</button>
+        <button type="submit" name="green">Verde</button>
+        <button type="submit" name="blue">Azul</button>
+    </form>
 </body>
 </html>
