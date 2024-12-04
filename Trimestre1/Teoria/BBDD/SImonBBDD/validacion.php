a6a011332ff674b77cf648378606bf1724c8ede0
Jordy <jordyl.reyesl@gmail.com>
2024-12-04 12:29:52 +0000

    Dec 4, 2024, 1:29 PM

--- Trimestre1/Teoria/BBDD/SImonBBDD/validacion.php
+++ Trimestre1/Teoria/BBDD/SImonBBDD/validacion.php
@@ -1,24 +1,26 @@
 <?php
     session_start();
-    require_once 'datdb.php';
+    $_SESSION['error']=0;
+    var_dump($_POST);
     
-    $_SESSION['error']=0;    
-
-    $ctdb=new mysqli($hn,$user,$pw,$db);
-    if($ctdb->connect_error)die("Error connecting");
-    else{
-    if(isset($_POST['enviar'])){
-        $usuarioValidar=$_POST['usuario'];
-        $claveValidar=$_POST['clave'];
-        $qrySelect="select Nombre,Clave from usuarios where Nombre= '$usuarioValidar' and Clave= '$claveValidar'";
-        $result=$ctdb->query($qrySelect);
-        $ctdb->close();
-        if($result->num_rows>0){
-            $_SESSION['usuario']=$usuarioValidar;
-            echo "inicio.php";
-        }else{
-            $_SESSION['error']=1;
-            echo $_SERVER['PHP_SELF'];
+    function validarUsuario(){
+        require_once 'datdb.php';
+        $ctdb=new mysqli($hn,$user,$pw,$db);
+        if($ctdb->connect_error)die("Error connecting");
+        else{
+        if(isset($_POST['enviar'])){
+            $usuarioValidar=$_POST['usuario'];
+            $claveValidar=$_POST['clave'];
+            $qrySelect="select Nombre,Clave from usuarios where Nombre= '$usuarioValidar' and Clave= '$claveValidar'";
+            $result=$ctdb->query($qrySelect);
+            $ctdb->close();
+            if($result->num_rows>0){
+                $_SESSION['usuario']=$usuarioValidar;
+                echo "inicio.php";
+            }else{
+                $_SESSION['error']=1;
+                echo $_SERVER['PHP_SELF'];
+            }
         }
     }
 }
