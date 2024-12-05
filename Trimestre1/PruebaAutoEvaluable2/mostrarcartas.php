d6f25882b3fbc5a61b8b4700c038e3e40c727b9c
Jordy <jordyl.reyesl@gmail.com>
2024-12-05 11:03:10 +0000

    Dec 5, 2024, 12:03 PM

--- Trimestre1/PruebaAutoEvaluable2/mostrarcartas.php
+++ Trimestre1/PruebaAutoEvaluable2/mostrarcartas.php
@@ -3,3 +3,14 @@
     $cartas=['copas_02.jpg','copas_03.jpg','copas_05.jpg'];
     
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juego</title>
+</head>
+<body>
+    <h1>Bienvenid@,<?php echo $_SESSION['login']?></h1>
+</body>
+</html>
