52cfb140826850cfbb0d2acb263421c95c226ff9
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 18:53:10 +0000

    Dec 11, 2024, 7:53 PM

--- Trimestre1/Teoria/Agenda/inicio.php
+++ Trimestre1/Teoria/Agenda/inicio.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $img=['OIP0.jfif','OIP1.jfif','OIP2.jfif','OIP3.jfif','OIP4.jfif'];
+    $img=['<img src="img/OIP0.jfif">','OIP1.jfif','OIP2.jfif','OIP3.jfif','OIP4.jfif'];
     if(!isset($_SESSION['cont'])){
         $_SESSION['imgs']=[$img[array_rand($img)],$img[array_rand($img)],$img[array_rand($img)],$img[array_rand($img)],$img[array_rand($img)]];
         $_SESSION['cont']=1;
@@ -15,17 +15,17 @@
 </head>
 <body>
     <h1>Agenda</h1>
-    <p>Hola<?php echo $_SESSION['usuario']?>, cuantos contactos deseas grabar?</p>
-    <p>puedes grabar entre 1 y 5. Por cada posicion en INCREMENTAR grabaras un usuario mas</p>
+    <p>Hola, cuantos contactos deseas grabar?</p>
+    <p>Puedes grabar entre 1 y 5. Por cada posicion en INCREMENTAR grabaras un usuario mas</p>
     <p>Cuando el numero sea el deseado pulsa GRABAR</p>
     <?php
         for($i=0;$i<$_SESSION['cont'];$i++){
-            echo `<img src='img/{$_SESSION['imgs']}'></img>`;
+            echo `<img src='img/{$_SESSION['imgs'][$i]}'>`;
         }
     ?>
     <form action="comprobar.php" method="post">
-        <input type="submit" value="incrementar" name="incrementar">
-        <input type="submit" value="grabar" name="grabar">
+        <input type="submit" value="Incrementar" name="incrementar">
+        <input type="submit" value="Grabar" name="grabar">
     </form>
 </body>
 </html>
