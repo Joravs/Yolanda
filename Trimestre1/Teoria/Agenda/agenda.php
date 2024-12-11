902002eb244491bb31cd2526a8683178f80a9ac9
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 18:59:05 +0000

    Dec 11, 2024, 7:59 PM

--- Trimestre1/Teoria/Agenda/agenda.php
+++ Trimestre1/Teoria/Agenda/agenda.php
@@ -1,4 +1,19 @@
 <?php
     session_start();
-    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+</head>
+<body>
+    <h1>Hola <?php $_SESSION['usuario']?></h1>
+    <?php
+        for($i=0;$i<$_SESSION['cont'];$i++){
+
+        }
+    ?>
+</body>
+</html>
