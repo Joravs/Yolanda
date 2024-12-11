90ffc8beec3c915d71d5988b40fe53bcf1758209
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 18:47:26 +0000

    Dec 11, 2024, 7:47 PM

--- Trimestre1/Teoria/Agenda/comprobar.php
+++ Trimestre1/Teoria/Agenda/comprobar.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
     if($_POST['incrementar']){
+        if($_SESSION['cont']==5){
+            header('Location: ');
+        }
         $_SESSION['cont']++;
         header('Location: inicio.php');
     }else{
