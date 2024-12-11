9ab1d7e1cc42780c4a47e4c05e61a9c7077965e9
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 18:02:22 +0000

    Dec 11, 2024, 7:02 PM

--- Trimestre1/Teoria/Agenda/inicio.php
+++ Trimestre1/Teoria/Agenda/inicio.php
@@ -1,3 +1,16 @@
 <?php
+    session_start();
     $img=['OIP0.jfif','OIP1.jfif','OIP2.jfif','OIP3.jfif','OIP4.jfif'];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AGENDA</title>
+</head>
+<body>
+    <h1>Agenda</h1>
+    <p>Hola<?php echo $_SESSION['login']?></p>
+</body>
+</html>
