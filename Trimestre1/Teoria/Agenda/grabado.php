e10a1b6c826ad75db2c35fd12aaff95e1791acc9
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 19:47:36 +0000

    Dec 11, 2024, 8:47 PM

--- Trimestre1/Teoria/Agenda/grabado.php
+++ Trimestre1/Teoria/Agenda/grabado.php
@@ -1,14 +1,13 @@
 <?php
     require_once 'datdb.php';
     session_start();
-    /* $ctdb=new mysqli($hn,$user,$pw,$db);
-    if($ctdb->connect_error) die("Error connecting"); */
-    /* $qryUsuario="Select codigo from usuarios where Nombre='{$_SESSION['usuario']}'";
+    $ctdb=new mysqli($hn,$user,$pw,$db);
+    if($ctdb->connect_error) die("Error connecting");
+    $qryUsuario="Select codigo from usuarios where Nombre='{$_SESSION['usuario']}'";
     $us=$ctdb->query($qryUsuario);
-    $codigousu=$us->fetch_assoc()['codigo']; */
-    $codigousu=1;
+    $codigousu=$us->fetch_assoc()['codigo'];
     for($i=1;$i<=$_SESSION['cont'];$i++){
-        echo $qryInsert="INSERT INTO contactos (nombre,email,telefono,codusuario) values ({$_POST['nombre'.$i]},{$_POST['email'.$i]},{$_POST['telefono'.$i]},$codigousu)";
+        $qryInsert="INSERT INTO contactos (nombre,email,telefono,codusuario) values ({$_POST['nombre'.$i]},{$_POST['email'.$i]},{$_POST['telefono'.$i]},$codigousu)";
+        $ctdb->query($qryInsert);
     };
-    echo $qryInsert;
 ?>
