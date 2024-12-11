d0bee2ec62ed2b92c1b815fc45472280c145fdd0
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 19:45:07 +0000

    Dec 11, 2024, 8:45 PM

--- Trimestre1/Teoria/Agenda/grabado.php
+++ Trimestre1/Teoria/Agenda/grabado.php
@@ -3,8 +3,13 @@
     session_start();
     /* $ctdb=new mysqli($hn,$user,$pw,$db);
     if($ctdb->connect_error) die("Error connecting"); */
+    /* $qryUsuario="Select codigo from usuarios where Nombre='{$_SESSION['usuario']}'";
+    $us=$ctdb->query($qryUsuario);
+    $codigousu=$us->fetch_assoc()['codigo']; */
+    $codigousu=1;
     $qryInsert="INSERT INTO contactos (nombre,email,telefono,codusuario) values ".
     for($i=1;$i<=$_SESSION['cont'];$i++){
-        echo "({$_POST['nombre'.$i]},${$_POST['email'.$i]},${$_POST['telefono'.$i]})";
+        echo "({$_POST['nombre'.$i]},${$_POST['email'.$i]},${$_POST['telefono'.$i]},$codigousu)";
     };
+    echo $qryInsert;
 ?>
