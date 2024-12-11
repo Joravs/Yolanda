d005c7ca70bd7eaa32b1f5e19c9f1a2da33132a9
Joravs <jordyl.reyesl@gmail.com>
2024-12-11 19:40:07 +0000

    Dec 11, 2024, 8:40 PM

--- Trimestre1/Teoria/Agenda/grabado.php
+++ Trimestre1/Teoria/Agenda/grabado.php
@@ -2,4 +2,9 @@
     require_once 'datdb.php';
     session_start();
     $ctdb=new mysqli($hn,$user,$pw,$db);
+    if($ctdb->connect_error) die("Error connecting");
+    $qryInsert="INSERT INTO contactos (nombre,email,telefono,codusuario) values ".
+    for($i=1;$i<=$_SESSION['cont'];$i++){
+
+    }
 ?>
