0beec1e35dec3fb0b6805b78a3edcf8b9d7bc680
Joravs <jordyl.reyesl@gmail.com>
2024-12-12 11:34:24 +0000

    Dec 12, 2024, 12:34 PM

--- Trimestre1/Jordy_Reyes/resultado.php
+++ Trimestre1/Jordy_Reyes/resultado.php
@@ -5,7 +5,7 @@
     $qryInsert="insert into respuestas(fecha,login,hora,respuesta) values($fecha,'{$_SESSION['uName']}',$hora,'{$_POST['respuesta']}')";
     $ctdb->query($qryInsert);
 
-    $qryAciertos="select login from jugador where (select respuesta from respuestas)in(select solucion from solucion) group by login";
+    $qryAciertos="select login,hora,count(solucion) from jugador where (select respuesta from respuestas)in(select solucion from solucion) group by login";
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,6 +16,6 @@
 </head>
 <body>
     <h1>Fecha: <?php echo $fecha;?></h1>
-    Jugadores acertantes:
+    <h2>Jugadores acertantes: </h2>
 </body>
 </html>
