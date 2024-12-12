19319c2df416a24333984e75cd3ff916b97792e1
Joravs <jordyl.reyesl@gmail.com>
2024-12-12 11:53:37 +0000

    Dec 12, 2024, 12:53 PM

--- Trimestre1/Jordy_Reyes/resultado.php
+++ Trimestre1/Jordy_Reyes/resultado.php
@@ -5,7 +5,10 @@
     $qryInsert="insert into respuestas(fecha,login,hora,respuesta) values($fecha,'{$_SESSION['uName']}',$hora,'{$_POST['respuesta']}')";
     $ctdb->query($qryInsert);
 
-    $qryAciertos="select login,hora,count(solucion) from jugador,solucion,respuestas where (select solucion from solucion where fecha={$fecha})=(select respuesta from respuestas where fecha={$fecha}) group by login";
+    $qryAciertos="select login,respuesta from respuestas,solucion where respuesta=(select solucion from solucion where fecha='2024-12-12') group by login";
+    $qryAciertos="select login,respuesta from respuestas,solucion where respuesta!=(select solucion from solucion where fecha='2024-12-12') group by login";
+    $result=$ctdb->query($qryAciertos);
+    $rows=$result->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,6 +19,24 @@
 </head>
 <body>
     <h1>Fecha: <?php echo $fecha;?></h1>
-    <h2>Jugadores acertantes: </h2>
+    <h2>Jugadores acertantes: <?php echo $rows?></h2>
+    <table>
+        <tr>
+            <th>Login</th>
+            <th>Hora</th>
+        </tr>
+    <?php 
+        for($i=0;$i<$rows;$i++){
+            $result->data_seek($i);
+            $row=$result->fetch_assoc();
+            echo "<tr>";
+            echo "<td>{$row['login']}</td>";
+            echo "<td>{$row['hora']}</td>";
+            echo "</tr>";
+        }
+    ?>
+    </table>
+    <h2>Jugadores que han fallado</h2>
+
 </body>
 </html>
