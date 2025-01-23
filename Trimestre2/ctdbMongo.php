c66b488b9b09f0a2730de5512f80659fd824475a
Jordy <jordyl.reyesl@gmail.com>
2025-01-23 11:29:10 +0000

    Jan 23, 2025, 12:29 PM

--- Trimestre2/ctdbMongo.php
+++ Trimestre2/ctdbMongo.php
@@ -3,11 +3,11 @@
     $ctdb = new MongoDB\Client("mongodb://localhost:27017");
     $db= $ctdb->Clase1;
     $clTest=$db->Test;
-    $datos=array("_id"=>1,"Nombre"=>"Jordy");
-    $clTest->insertOne($datos);
+    /* $datos=array("_id"=>1,"Nombre"=>"Jordy");
+    $clTest->insertOne($datos); */
 
     $result=$clTest->find(["Nombre"=>"Jordy"]);
     foreach($result as $r){
-        echo $r["ID"]."<br>";
-    }    
+        echo $r["Nombre"]."<br>";
+    }
 ?>
