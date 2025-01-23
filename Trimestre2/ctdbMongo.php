7669fdc1774d8bc7553e0171882412707a70b54a
Jordy <jordyl.reyesl@gmail.com>
2025-01-23 11:15:21 +0000

    Jan 23, 2025, 12:15 PM

--- Trimestre2/ctdbMongo.php
+++ Trimestre2/ctdbMongo.php
@@ -1,5 +1,9 @@
 <?php
     require_once "C:/xampp/php/vendor/autoload.php";
-    $ctdb = new MongoDB\Client("mongodb:localhost:27017");
-    if($ctdb->error)
+    $ctdb = new MongoDB\Client("mongodb://localhost:27017");
+    $db= $ctdb->Clase1;
+    $clTest=$db->Test;
+    $datos=array("ID"=>1,"Nombre"=>"Jordy");
+    $clTest->insertOne($datos);
+    
 ?>
