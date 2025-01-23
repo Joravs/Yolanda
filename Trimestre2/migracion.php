af3de427cf507e9b66c7d3c8bc51f1c6ff0a9bb1
Jordy <jordyl.reyesl@gmail.com>
2025-01-23 12:31:17 +0000

    Jan 23, 2025, 1:31 PM

--- Trimestre2/migracion.php
+++ Trimestre2/migracion.php
@@ -4,7 +4,7 @@
 
     $selectUsers="SELECT nombre,apellido1,apellido2,departamento FROM empleados";
     $result=$conn->query($selectUsers);
-    if($result->rows>0){
+    if($result->num_rows>0){
         foreach($result as $r){
             $dato=array("nombre"=>$r["nombre"],"apellido1"=>$r["apellido1"],"apellido2"=>$r["apellido2"],"departamento"=>$r["departamento"]);
             $clTest->insertOne($dato);
