a151b97daaae4c08c7de02b53d6a1be23a61de6c
Jordy <jordyl.reyesl@gmail.com>
2025-01-23 12:47:22 +0000

    Jan 23, 2025, 1:47 PM

--- Trimestre2/Migracion/migracion.php
+++ Trimestre2/Migracion/migracion.php
@@ -1,6 +1,6 @@
 <?php
-    require_once "ctdbMysql.php";
-    require_once "ctdbMongo.php";
+    require_once "../ctdbMysql.php";
+    require_once "../ctdbMongo.php";
 
     $selectUsers="SELECT nombre,apellido1,apellido2,departamento FROM empleados";
     $result=$conn->query($selectUsers);
---
+++ Trimestre2/Tarea/datosGijon.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "../ctdbMongo.php";
+
+    $JSONtoBSON=MongoDB\BSON\fromJSON("");
+?>
