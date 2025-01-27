effda29b71bd6da775c7bc49df595657f856b291
Jordy <jordyl.reyesl@gmail.com>
2025-01-27 13:24:28 +0000

    Jan 27, 2025, 2:24 PM

--- Trimestre2/Tarea/eliminarDato.php
+++ Trimestre2/Tarea/eliminarDato.php
@@ -1,11 +1,32 @@
 <?php
-    require_once "../ctdbMongo.php";
+    if(isset($_POST)){
+        require_once "../ctdbMongo.php";
+        if ($_POST['submit']=="eliminar"){
+            $dato=["calle"=>$_POST["calle"]];
+            $clCalles->find($dato);
+            
+        }else{
 
-    $dato=["calle"=>"LLOREU"];
-    $result=$clCalles->find($dato);
-
-    foreach($result as $calle){
-        echo $calle;
+        }
+    }else{
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHPSELF'];?>" method="post">
+        <legend>Introduce la calle que quieres eliminar o modificar</legend>
+        <label for="calle">Calle</label>
+        <input type="text" name="calle" id="calle">
+        <input type="submit" name="submit" value="eliminar">
+        <input type="submit" name="submit" value="modificar">
+    </form>
+</body>
+</html>
+<?php
     }
-    /* $clCalles->deleteOne($dato); */
 ?>
