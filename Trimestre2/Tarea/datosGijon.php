d7630311950d253ed5a2b4a4ba5cce85185388c8
Jordy <jordyl.reyesl@gmail.com>
2025-01-27 12:23:38 +0000

    Jan 27, 2025, 1:23 PM

--- Trimestre2/Tarea/datosGijon.php
+++ Trimestre2/Tarea/datosGijon.php
@@ -4,21 +4,22 @@
 
     $json=file_get_contents("callesgijon.json");
     $datos=json_decode($json,true);
-    foreach($datos as $dat){
+    /* foreach($datos as $dat){
         foreach($dat as $k){
             foreach($k as $v){
                 $clCalles->insertOne($v);
             }
         }
-    }
+    } */
+    $clCalles->insertMany($datos);
 
-    $datos=$clCalles->find();
+/*     $datos=$clCalles->find();
     $cont=0;
     foreach($datos as $dat){
         $conn->query("INSERT INTO calles (nombre,calle,tipo,numeroaccesos) VALUES ('{$dat['nombre']}', '{$dat['calle']}', '{$dat['tipo']}', '{$dat['nÚmeroaccesos']}')");
         $cont++;
     }
-    echo "Datos introducidos".$cont;
+    echo "Datos introducidos".$cont; */
 
     $conn->close;
     $ctdb->close;
