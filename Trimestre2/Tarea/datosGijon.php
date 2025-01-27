4fc1a3f228eeac6ae68972c368ba0acb18a5b0c2
Jordy <jordyl.reyesl@gmail.com>
2025-01-27 12:28:41 +0000

    Jan 27, 2025, 1:28 PM

--- Trimestre2/Tarea/datosGijon.php
+++ Trimestre2/Tarea/datosGijon.php
@@ -5,12 +5,11 @@
     $json=file_get_contents("callesgijon.json");
     $datos=json_decode($json,true);
     foreach($datos as $dat){
-        /* foreach($dat as $k){
+        foreach($dat as $k){
             foreach($k as $v){
                 $clCalles->insertOne($v);
             }
-        } */
-        $clCalles->insertMany($dat);
+        }
     }
 
     $datos=$clCalles->find();
