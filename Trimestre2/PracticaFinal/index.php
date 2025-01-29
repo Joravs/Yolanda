3290acdf1e4aefd6143c288be5772e2db6a1c791
Jordy <jordyl.reyesl@gmail.com>
2025-01-29 12:03:42 +0000

    Jan 29, 2025, 1:03 PM

---
+++ Trimestre2/PracticaFinal/index.php
@@ -0,0 +1,43 @@
+<?php
+    
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Gestor de Administracion de Insulina</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <div class="row text-center">
+                <h1>Gestor de Administracion de Insulina</h1>
+            </div>
+            <div
+                class="table-responsive"
+            >
+                <table class="table table-striped table-hover table-borderless table-primary align-middle">
+                    <thead class="table-light">
+                        <caption>Insulina</caption>
+                        <tr>
+                            <th></th>
+                            <th>Desayuno</th>
+                            <th>Comida</th>
+                            <th>Cena</th>
+                            <th>Deporte</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        
+                    </tbody>
+                    <tfoot>
+                        
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
