865c4bb9336bf1225fdec9b68573a01aa48f9ccf
Joravs <jordyl.reyesl@gmail.com>
2025-01-30 16:42:03 +0000

    Jan 30, 2025, 5:42 PM

--- Trimestre2/PracticaFinal/index.php
+++ Trimestre2/PracticaFinal/index.php
@@ -18,6 +18,8 @@
                 <label for="passwd" class="form-label text-primary">Contraseña</label>
                 <input type="password" class="form-control" name="passwd" id="passwd" placeholder="Password"/>
             </div>
+            <button type="button" class="btn btn-primary btn-md">Iniciar Sesion</button>
+            
         </div>
     </div>
 </body>
