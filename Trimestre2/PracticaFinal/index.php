26e64cbfc640b79669e2656017c60b3c6062c8e3
Joravs <jordyl.reyesl@gmail.com>
2025-01-30 16:47:38 +0000

    Jan 30, 2025, 5:47 PM

--- Trimestre2/PracticaFinal/index.php
+++ Trimestre2/PracticaFinal/index.php
@@ -10,7 +10,7 @@
 <body class="bg-secondary">
     <div class="container-fluid">
         <div id="login" class="rounded bg-light d-flex flex-column justify-content-center align-item-center mx-auto p-4" style="width: 22rem; margin-top:35vh;">
-            <form class="row g-3 needs-validation" novalidate>
+            <form class="row g-3 needs-validation" method="post" action="validar.php">
               <div class="mb-3 row-12 text-center">
                 <label for="username" class="form-label text-primary">Usuario</label>
                 <input type="text" class="form-control" name="username" id="username" placeholder="Username"/>
@@ -21,8 +21,9 @@
             </div>
             <button type="submit" class="btn btn-primary btn-md">Iniciar Sesion</button>
             </form>
-            <button type="submit" class="btn btn-primary btn-md">Iniciar Sesion</button>
-            
+            <form class="row mt-3 needs-validation p-1" method="post" action="registro.php">
+                <button type="submit" class="btn btn-primary btn-md">Registrarse</button>
+            </form>
         </div>
     </div>
 </body>
