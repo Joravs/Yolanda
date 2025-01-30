e39cf471c101843b7fa8629fa52246ecf05396a2
Joravs <jordyl.reyesl@gmail.com>
2025-01-30 16:41:30 +0000

    Jan 30, 2025, 5:41 PM

--- Trimestre2/PracticaFinal/index.php
+++ Trimestre2/PracticaFinal/index.php
@@ -9,13 +9,13 @@
 </head>
 <body class="bg-secondary">
     <div class="container-fluid">
-        <div id="login" class="rounded bg-light d-flex flex-column justify-content-center align-item-center mt-5 p-4">
+        <div id="login" class="rounded bg-light d-flex flex-column justify-content-center align-item-center mx-auto p-4" style="width: 22rem; margin-top:35vh;">
             <div class="mb-3 row-12 text-center">
-                <label for="username" class="form-label text-info">Usuario</label>
+                <label for="username" class="form-label text-primary">Usuario</label>
                 <input type="text" class="form-control" name="username" id="username" placeholder="Username"/>
             </div>
             <div class="mb-3 row-12 text-center">
-                <label for="passwd" class="form-label text-info">Contraseña</label>
+                <label for="passwd" class="form-label text-primary">Contraseña</label>
                 <input type="password" class="form-control" name="passwd" id="passwd" placeholder="Password"/>
             </div>
         </div>
