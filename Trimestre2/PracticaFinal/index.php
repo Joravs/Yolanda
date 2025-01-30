1d4c73777e25c9bb09905514d30b319fcf4bf262
Joravs <jordyl.reyesl@gmail.com>
2025-01-30 16:15:02 +0000

    Jan 30, 2025, 5:15 PM

--- Trimestre2/PracticaFinal/index.php
+++ Trimestre2/PracticaFinal/index.php
@@ -10,6 +10,10 @@
 <body>
     <div class="container-fluid">
         <div id="login" class="d-flex justify-content-center ialign-item-center">
+            <div class="mb-3">
+                <label for="" class="form-label">Password</label>
+                <input type="password" class="form-control" name="" id="" placeholder=""/>
+            </div>
             
         </div>
     </div>
