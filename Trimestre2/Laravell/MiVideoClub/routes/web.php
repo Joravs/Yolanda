5ecaff34700d6c0874dcdf38da880c3655632ae7
Jordy <jordyl.reyesl@gmail.com>
2025-02-10 14:15:20 +0100

    Changes

--- Trimestre2/Laravell/MiVideoClub/routes/web.php
+++ Trimestre2/Laravell/MiVideoClub/routes/web.php
@@ -6,3 +6,15 @@ Route::get('/', function () {
     //return view('welcome');
     return "Hello, World!";
 });
+
+Route::get('pagina1', function () {
+    return "Estas en la pagina 1";
+});
+
+Route::get('pagina2/{id}', function ($id){
+    return 'Usuario '.$id;
+});
+
+Route::get('pagina3/{nombre?}', function ($nombre = "null"){
+    return 'Hola, '.$nombre;
+});
