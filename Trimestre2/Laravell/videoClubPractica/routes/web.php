a5e417876fd35420f975305e6517a7720d429978
Jordy <jordyl.reyesl@gmail.com>
2025-02-12 12:38:59 +0000

    Feb 12, 2025, 1:38 PM

--- Trimestre2/Laravell/videoClubPractica/routes/web.php
+++ Trimestre2/Laravell/videoClubPractica/routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/', function () {
+    return view('auth.login');
+});
+Route::get('/', function () {
+    return view('catalog.index');
+});
+Route::get('/', function () {
+    return view('catalog.show');
+});
+Route::get('/', function () {
+    return view('catalog.create');
+});
+Route::get('/', function () {
+    return view('catalog.edit');
 });
