9a86ad8b51a39de9306dd5fe293952d2f31c5d42
Jordy <jordyl.reyesl@gmail.com>
2025-02-26 13:04:55 +0000

    Feb 26, 2025, 2:04 PM

--- Trimestre2/Laravell/videoClubPractica/resources/views/catalog/index.blade.php
+++ Trimestre2/Laravell/videoClubPractica/resources/views/catalog/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.master')
     @section('content')
     <h1>Listado de Peliculas</h1>
+    <div class="row my-1 fs-5">
     @foreach($peliculas as $key=>$pelicula)
-    <div class="row bg-dark my-1 text-white fs-5">
-        <div.col-2>
-        <h3>Titulo: {{$pelicula['title']}}</h3>
-        <h3>Año: {{$pelicula['year']}}</h3>
-        <h3>Director: {{$pelicula['director']}}</h3>
-        </div.col-2>
-        <img src="{{$pelicula['poster']}}"></img>
-        <p>{{$pelicula['rented']}}</p>
-        <p>{{$pelicula['synopsis']}}</p>
+        <div class='col-2'>
+            <h3>Titulo: {{$pelicula['title']}}</h3>
+            <h3>Año: {{$pelicula['year']}}</h3>
+            <h3>Director: {{$pelicula['director']}}</h3>
+            <img src="{{$pelicula['poster']}}" style="width:400px;height:auto;"></img>
+            <p>{{$pelicula['rented']}}</p>
+            <p>{{$pelicula['synopsis']}}</p>
+        </div>
+        @endforeach
     </div>
-    @endforeach
 @stop
--- Trimestre2/Laravell/videoClubPractica/resources/views/layouts/master.blade.php
+++ Trimestre2/Laravell/videoClubPractica/resources/views/layouts/master.blade.php
@@ -4,23 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
-    <!--<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
-	<!--<link href="assets/bootstrap/js/bootstrap.min.js" rel="stylesheet">-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap.min.js') }}">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-	
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+	  <link rel="stylesheet" type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+	  <link rel="stylesheet" type="text/css" href="{{ asset('assets/bootstrap/css/bootstrap.min.js') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
     @include('partials.navbar')
