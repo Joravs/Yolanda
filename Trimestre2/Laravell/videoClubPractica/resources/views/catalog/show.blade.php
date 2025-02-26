f0e076aa113f1ad3425250e060f50778f5943f55
Jordy <jordyl.reyesl@gmail.com>
2025-02-26 14:17:45 +0100

    Changes

--- Trimestre2/Laravell/videoClubPractica/resources/views/catalog/show.blade.php
+++ Trimestre2/Laravell/videoClubPractica/resources/views/catalog/show.blade.php
@@ -6,4 +6,5 @@
     <h3>Año: {{$pelicula['year']}}</h3>
     <h3>Sinopsis: </h3>
     <p>{{$pelicula['synopsis']}}</p>
+    <a href="/Yolanda/Trimestre2/Laravell/videoClubPractica/public/catalog" class="fs-3">Volver</a>
 @stop
