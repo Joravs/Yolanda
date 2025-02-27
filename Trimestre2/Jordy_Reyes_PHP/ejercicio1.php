4d173a1dcb72c35d34804cb4848885db4f542aaf
Joravs <jordyl.reyesl@gmail.com>
2025-02-27 11:29:32 +0000

    Feb 27, 2025, 12:29 PM

--- Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
+++ Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pictograma</title>
+    <style>
+        img{
+            width: 300px;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
     <div class="container-fluid">
@@ -22,8 +28,8 @@
         }
         $saltoLinea++;
         echo "<td>
-                <div class='row'><img src='{$img}'></div>
-                <div class='row'>{$img}</div>
+                <div><img src='{$img}'></div>
+                <div>{$img}</div>
               </td>";
         if($saltoLinea==4){
             $saltoLinea=0;
