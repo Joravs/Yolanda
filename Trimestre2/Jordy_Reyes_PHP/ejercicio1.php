6b395736d027256917d3d215858fc4a8cf2927fa
Joravs <jordyl.reyesl@gmail.com>
2025-02-27 11:10:14 +0000

    Feb 27, 2025, 12:10 PM

--- Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
+++ Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+    <?php
     require_once './ctdb.php';
 
     $slct = $conn -> prepare ('SELECT imagen from imagenes');
@@ -6,10 +15,17 @@
     $slct->store_result();
 
     if($slct->num_rows > 0) {
-?>
+        $rows=$slct->num_rows;
+        for($i=0;$i<$rows;$i++){
+            $img=$slct->fetch_assoc();
 
+        }
+?>
 <?php
     }else{
         echo '<div>No se encontraron Imagenes</div>';
     }
 ?>
+    </div>    
+</body>
+</html>
