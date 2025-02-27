7b2882e90bf7c9fa6c94d60605cf922faa417ff3
Joravs <jordyl.reyesl@gmail.com>
2025-02-27 11:34:28 +0000

    Feb 27, 2025, 12:34 PM

---
+++ Trimestre2/Jordy_Reyes_PHP/ejercicio2.php
@@ -0,0 +1,26 @@
+<?php
+    require_once './ctdb.php';
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pictograma</title>
+    <style>
+        img{
+            width: 200px;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <h3>Añadir datos agenda</h3>
+        <form action="<?= $_SERVER['PHPSELF'];?>">
+
+        </form>
+    </div>    
+</body>
+</html>
