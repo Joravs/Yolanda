bb20caa5ed0ee7b713d22f3aeef8e81dbbb2f6a9
Joravs <jordyl.reyesl@gmail.com>
2025-02-27 12:18:47 +0000

    Feb 27, 2025, 1:18 PM

--- Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
+++ Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
@@ -6,12 +6,12 @@
     <title>Pictograma</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
     <style>
-        td{
-        align-items:baseline; 
-        }
         img{
             width: 200px;
             height: auto;
+        }
+        table,td{
+            border:1px solid #000;
         }
     </style>
 </head>
