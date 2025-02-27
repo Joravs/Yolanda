64aaae6c4825887bfed19907944409040641baea
Joravs <jordyl.reyesl@gmail.com>
2025-02-27 11:05:56 +0000

    Feb 27, 2025, 12:05 PM

--- Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
+++ Trimestre2/Jordy_Reyes_PHP/ejercicio1.php
@@ -1,4 +1,11 @@
 <?php
     require_once './ctdb.php';
-    
+
+    $slct = $conn -> prepare ('SELECT imagen from imagenes');
+    $slct-> execute();
+    $slct->store_result();
+
+    if($slct){
+
+    }
 ?>
