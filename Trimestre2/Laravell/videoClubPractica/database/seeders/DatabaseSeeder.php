65b2554e87225f1821a5c9fbc326777141e25d7c
Jordy <jordyl.reyesl@gmail.com>
2025-03-13 14:11:54 +0100

    Changes

--- Trimestre2/Laravell/videoClubPractica/database/seeders/DatabaseSeeder.php
+++ Trimestre2/Laravell/videoClubPractica/database/seeders/DatabaseSeeder.php
@@ -203,7 +203,7 @@ class DatabaseSeeder extends Seeder
 			"title" => "Blade Runner",
 			"year" => "1982",
 			"director" => "Ridley Scott",
-			"poster" => "https://media.giphy.com/media/VET3tgDayFtpgBnbRH/giphy.gif?cid=790b7611n8wcpb5jmd2uy4xosfo4pnqmujj7j640fz01g1ll&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+			"poster" => "https://media.giphy.com/media/cITQQDTqzwf65rOLIy/giphy.gif?cid=790b7611lvr2s1mtj6d8aj3t4yt8vs26cduu2shaiglcs6ck&ep=v1_gifs_search&rid=giphy.gif&ct=g",
 			"rented" => "1",
 			"synopsis" => "A principios del siglo XXI, la poderosa Tyrell Corporation creó, gracias a los avances de la ingeniería genética, un robot llamado Nexus 6, un ser virtualmente idéntico al hombre pero superior a él en fuerza y agilidad, al que se dio el nombre de Replicante. Estos robots trabajaban como esclavos en las colonias exteriores de la Tierra. Después de la sangrienta rebelión de un equipo de Nexus-6, los Replicantes fueron desterrados de la Tierra. Brigadas especiales de policía, los Blade Runners, tenían órdenes de matar a todos los que no hubieran acatado la condena. Pero a esto no se le llamaba ejecución, se le llamaba \"retiro\".",
 			"created_at" => "2025-03-06 13:10:47",
@@ -243,7 +243,7 @@ class DatabaseSeeder extends Seeder
 			"updated_at" => "2025-03-13 12:40:28"
 		]
 	];
-	
+
     public function run(): void
     {
         self::seedCatalog();
