557175f0c8ad3657cc49d4dd3a60a60b71526381
Joravs <jordyl.reyesl@gmail.com>
2025-03-20 21:53:20 +0100

    Changes

--- Trimestre2/Laravell/Pictograma/routes/web.php
+++ Trimestre2/Laravell/Pictograma/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImagenesController;
 use App\Http\Controllers\AgendaController;
 
+
+Route::get('/', [ImagenesController::class, 'getContent']);
 Route::get('/catalog', [ImagenesController::class, 'getContent']);
 Route::get('/agenda/add', [AgendaController::class, 'getAdd'])->name('agenda.add');
 Route::post('/agenda/add', [AgendaController::class, 'postAdd']);
